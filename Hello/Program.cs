276ce931d982f6b02a7d2a817a86919b0ff78940
Angelika Ryngier <angelika2804@gmail.com>
2019-07-12 16:26:13 +0200

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];
+            Console.WriteLine("Hello, " + name);
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Hello, " + item);
+            //}
+        }
+    }
+}
