a0d54f0fd76fbcf3aa9ba42fbe6f1b3d87cdb92c
Angelika Ryngier <angelika2804@gmail.com>
2019-07-12 17:17:35 +0200

    Pierwszy program Hello. Tablica args

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -8,7 +8,7 @@ namespace Hello
         static void Main(string[] args)
         {
             string name = args[0];
-            Console.WriteLine("Hello, " + name); 
+            Console.WriteLine("Hello, " + name); //zmiana
             //foreach (var item in args)
             //{
             //    Console.WriteLine("Hello, " + item);
